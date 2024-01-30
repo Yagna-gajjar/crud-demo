85b9aca253e762dfaba1bc9cd323858d3f71d593
Yagna-gajjar <yagnagangajaliya@gmail.com>
2024-01-30 19:25:33 +0530

    crud-app

---
+++ crud/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import route from "./routes/userRoute.js";
+import path from "path";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors());
+dotenv.config();
+
+const mongourl = process.env.MONGOURL
+
+mongoose.connect(mongourl).then(() => {
+
+    console.log("DB connected");
+
+    app.listen(5000, () => {
+        console.log(`server is running on port: 5000`);
+    })
+
+}).catch(error => console.log(error));
+
+const _dirname = path.resolve();
+app.use("/api", route);
+app.use((express.static(path.join(_dirname, '/client/build'))))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, 'client', 'build', 'index.html'))
+})
---
+++ crud/model/userModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true
+    }, Lname: {
+        type: String,
+        required: true
+    }, email: {
+        type: String,
+        required: true
+    }, password: {
+        type: String,
+        required: true
+    },
+})
+
+export default mongoose.model("User", userSchema); 
